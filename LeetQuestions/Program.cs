d23b8504897807a217fc3789ec9d954b74eb7b5f
Kin <kingglu684@gmail.com>
2026-02-08 07:42:17 -0500

    First Publish

---
+++ LeetQuestions/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetQuestions;
+
+class LeetQuestions
+{
+    public static void Main(string[] args)
+    {
+        HelloWorldPrompt("Hello, World!");
+
+        
+    }
+
+
+    public static void HelloWorldPrompt(string text)
+    {
+        System.Console.WriteLine(text); 
+    }
+
+
+
+}
